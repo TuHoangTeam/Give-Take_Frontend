dd67a9283b7578bedf62d63417f3c3ca3d4340d2
Ngô Ngọc Triệu Mẫn <meomeochitchithihi@gmail.com>
2025-12-11 01:02:20 +0700

    feat(Setup): :art: setup sentry

--- App/metro.config.js
+++ App/metro.config.js
@@ -1,8 +1,8 @@
-const { withNativeWind } = require('nativewind/metro');
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+const { withNativeWind } = require("nativewind/metro");
 
-module.exports = withNativeWind(config, { input: './global.css' });
+let config = getSentryExpoConfig(__dirname);
+config = withNativeWind(config, { input: "./global.css" });
+
+module.exports = config;
